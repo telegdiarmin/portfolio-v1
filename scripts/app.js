ec4084206e8563a845baaf762f775f5582809d78
telegdiarmin <armin.telegdi@gmail.com>
2022-05-02 12:39:03 +0200

    Updated CV, profile picture and skills. Added scroll behavior to the scrollmark at the bottom of the first section.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,7 @@
 const dropdownContentElement = document.getElementById("app-dropdown-cont");
 const dropdownTabElement = document.getElementById("app-dropdown");
+const scrollmark = document.getElementById("scrollmark")
+const aboutSection = document.getElementById("about")
 
 function toggleDropdown() {
   dropdownContentElement.classList.toggle("dropdown-show");
@@ -11,5 +13,11 @@ function closeDropdown(event){
     console.log("Dropdown is hidden!")
 }
 
+function jumpToAboutSection(event){
+  aboutSection.scrollIntoView();
+}
+
 dropdownTabElement.addEventListener("click", toggleDropdown);
-document.addEventListener("click", closeDropdown)
+document.addEventListener("click", closeDropdown);
+scrollmark.addEventListener("click", jumpToAboutSection)
+
